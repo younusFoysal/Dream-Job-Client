88b7a06e7ae948e466fc4d3694a152e877e0a59f
younusFoysal <younus.faysal@gmail.com>
2024-05-11 01:49:14 +0600

    Card design fix

--- src/pages/Jobs/JobsCategory.jsx
+++ src/pages/Jobs/JobsCategory.jsx
@@ -30,8 +30,8 @@ const JobsCategory = () => {
                     <TabPanel>
 
                         {/*job Card*/}
-                        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-                            <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+                        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden  py-6 sm:py-12">
+                            <div className="group relative cursor-pointer overflow-hidden px-6 pt-10 pb-8 bg-gray-50 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
                                 <span className="absolute top-10 z-0 h-10 w-10 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[20]"></span>
                                 <div className="relative z-10 mx-auto max-w-md ">
                                     <span className="grid h-10 w-10 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
