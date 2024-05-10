f1ae7f5702c68ae65ff558f5dde0b0fd0278e842
younusFoysal <younus.faysal@gmail.com>
2024-05-10 13:14:39 +0600

    Login and Register Start

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createBrowserRouter} from "react-router-dom";
 import Main from "../Layout/Main.jsx";
 import Home from "../pages/Home/Home.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     },
